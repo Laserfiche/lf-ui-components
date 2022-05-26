249a3827564d79d7297985289d0568223c33538c
Ke Jin <ke.jin@laserfiche.com>
2022-05-26 09:19:25 -0700

    add getting started cdn and npm page

---
+++ projects/lf-documentation/src/app/getting-started-cdn-example/getting-started-cdn-example.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-getting-started-cdn-example',
+  templateUrl: './getting-started-cdn-example.component.html',
+  styleUrls: ['./getting-started-cdn-example.component.css', './../app.component.css']
+})
+export class GettingStartedCdnExampleComponent {
+
+  constructor() { }
+}
