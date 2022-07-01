8e89bc0fe295499b5fa9015ed832c495cbcd1643
Ke Jin <ke.jin@laserfiche.com>
2022-07-01 09:48:50 -0700

    add ng-package

--- projects/ui-components/entry.ts
+++ projects/ui-components/entry.ts
@@ -2,4 +2,4 @@
 // be imported through their individual entry-points. This file is needed to
 // satisfy the "ng_package" bazel rule which also requires a primary entry-point.
 
-export const dummyVar = true;
+export const dummyVar = "@laserfiche/lf-ui-components";
