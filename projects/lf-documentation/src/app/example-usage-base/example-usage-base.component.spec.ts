b66e2746464f5bbd83766da87627c63789598ff9
Ke Jin <ke.jin@laserfiche.com>
2022-07-18 11:52:56 -0700

    fix ng test

--- projects/lf-documentation/src/app/example-usage-base/example-usage-base.component.spec.ts
+++ projects/lf-documentation/src/app/example-usage-base/example-usage-base.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ExampleUsageBaseComponent } from './example-usage-base.component';
 
@@ -8,7 +9,8 @@ describe('ExampleUsageBaseComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExampleUsageBaseComponent ]
+      imports: [RouterTestingModule],
+      declarations: [ ExampleUsageBaseComponent ],
     })
     .compileComponents();
   });
