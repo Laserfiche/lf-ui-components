167c6c2a9933ea663228b1a3b96f80d85725e2c7
Ke Jin <ke.jin@laserfiche.com>
2023-03-15 18:51:11 -0700

    fix test

--- projects/ui-components/lf-user-feedback/feedback-submission/feedback-submission.component.spec.ts
+++ projects/ui-components/lf-user-feedback/feedback-submission/feedback-submission.component.spec.ts
@@ -4,21 +4,34 @@ import { FeedbackSubmissionComponent } from './feedback-submission.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { By } from '@angular/platform-browser';
 import { LfMessageToastTypes } from '../lf-toast-message/lf-toast-message.component';
+import { AppLocalizationService } from '@laserfiche/lf-ui-components/internal-shared';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'lf-feedback-image-upload',
   template: '<p>Mock Image Attach Component</p>',
 })
-class MockFeedbackImageUploadComponent {}
+class MockFeedbackImageUploadComponent { }
 
 describe('FeedbackSubmissionComponent', () => {
   let component: FeedbackSubmissionComponent;
   let fixture: ComponentFixture<FeedbackSubmissionComponent>;
-
+  let localizeServiceMock: jasmine.SpyObj<AppLocalizationService>;
   beforeEach(async () => {
+    localizeServiceMock = jasmine.createSpyObj('localization',
+      ['getStringLaserficheObservable', 'getStringComponentsObservable']);
+    localizeServiceMock.getStringLaserficheObservable.and.callFake((value: string) => {
+      return of(value);
+    });
+    localizeServiceMock.getStringComponentsObservable.and.callFake((value: string) => {
+      return of(value);
+    });
     await TestBed.configureTestingModule({
       declarations: [FeedbackSubmissionComponent, MockFeedbackImageUploadComponent],
       imports: [MatCheckboxModule],
+      providers: [
+        { provide: AppLocalizationService, useValue: localizeServiceMock },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FeedbackSubmissionComponent);
@@ -30,7 +43,7 @@ describe('FeedbackSubmissionComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('if the feedback image upload emits error, should set toastMessages',() => {
+  it('if the feedback image upload emits error, should set toastMessages', () => {
     const mockImageUploadComponent = fixture.debugElement.query(By.directive(MockFeedbackImageUploadComponent));
     const testErrorMessage = 'test error';
     mockImageUploadComponent.triggerEventHandler('imageUploadError', testErrorMessage);
@@ -45,16 +58,16 @@ describe('FeedbackSubmissionComponent', () => {
     ]);
   });
 
-  it('if the feedback image upload emits feedbackImageBase64, should set feedbackImageBase64',() => {
+  it('if the feedback image upload emits feedbackImageBase64, should set feedbackImageBase64', () => {
     const mockImageUploadComponent = fixture.debugElement.query(By.directive(MockFeedbackImageUploadComponent));
     const testBase64 = 'test base64';
     mockImageUploadComponent.triggerEventHandler('feedbackImageBase64', testBase64);
     expect(component.feedbackImageBase64).toEqual(testBase64);
   });
 
-  it('if textarea input changes, should emit feedbackTextChanged',() => {
+  it('if textarea input changes, should emit feedbackTextChanged', () => {
     spyOn(component.feedbackTextChanged, 'emit');
-    const textAreaInput  = document.getElementsByTagName('textarea')[0];
+    const textAreaInput = document.getElementsByTagName('textarea')[0];
     const testInput = 'test input';
     textAreaInput.value = testInput;
     textAreaInput.dispatchEvent(new Event('input'));
@@ -62,23 +75,21 @@ describe('FeedbackSubmissionComponent', () => {
     expect(component.feedbackLength).toEqual(testInput.length);
   });
 
-  it('if is feedback, should display feedback string',() => {
-    // TODO: mock localizationService and return the key
+  it('if is feedback, should display feedback string', () => {
     component.isFeedback = true;
     fixture.detectChanges();
     const feedbackDescription = fixture.debugElement.query(By.css('.lf-user-feedback-description'));
-    expect(feedbackDescription.nativeElement.innerText).toBe('Found something you like or dislike? Let us know about it.');
+    expect(feedbackDescription.nativeElement.innerText).toBe('FOUND_SOMETHING_LIKE_DISLIKE_LET_US_KNOW');
     const textLabel = fixture.debugElement.query(By.css('.lf-text-label'));
-    expect(textLabel.nativeElement.innerText).toBe('Tell us about your experience');
+    expect(textLabel.nativeElement.innerText).toBe('TELL_US_ABOUT_EXPERIENCE (REQUIRED)');
   });
 
-  it('if is not feedback, should display suggestion string',() => {
-    // TODO: mock localizationService and return the key
+  it('if is not feedback, should display suggestion string', () => {
     component.isFeedback = false;
     fixture.detectChanges();
     const feedbackDescription = fixture.debugElement.query(By.css('.lf-user-feedback-description'));
-    expect(feedbackDescription.nativeElement.innerText).toBe('Do you have an idea for a new feature or an improvement? We look forward to hearing about it.');
+    expect(feedbackDescription.nativeElement.innerText).toBe('DO_YOU_HAVE_IDEA_NEW_FEATURE_IMPROVEMENT_LOOK_FORWARD_TO_HEARING');
     const textLabel = fixture.debugElement.query(By.css('.lf-text-label'));
-    expect(textLabel.nativeElement.innerText).toBe('Tell us about your idea');
+    expect(textLabel.nativeElement.innerText).toBe('TELL_US_ABOUT_IDEA (REQUIRED)');
   });
 });
