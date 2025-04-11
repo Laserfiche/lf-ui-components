b7de053d326761251008a5041e7b67e1cfd44669
Shiyuan Zhao <shiyuan.zhao@laserfiche.com>
2025-04-11 11:06:43 -0700

    fix insecure randomness

--- projects/ui-components/lf-metadata/lf-date-time-picker/uni-date-time.service.ts
+++ projects/ui-components/lf-metadata/lf-date-time-picker/uni-date-time.service.ts
@@ -534,7 +534,9 @@ export class UniDateTimeService {
       d += window.performance.now(); //use high-precision timer if available
     }
     const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      const r = (d + Math.random() * 16) % 16 | 0;
+      const randomValues = new Uint8Array(1);
+      window.crypto.getRandomValues(randomValues);
+      const r = (d + randomValues[0] % 16) % 16 | 0;
       d = Math.floor(d / 16);
       return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
     });
