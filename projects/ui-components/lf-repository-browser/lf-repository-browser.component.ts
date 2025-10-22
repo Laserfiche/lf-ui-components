a3ac474962e029d27f0f61f3d3e57757eaf97142
Levente Juhasz <juhasz.levente-contractor@laserfiche.com>
2025-10-22 20:08:20 +0300

    Retain keyboard focus inside the repository browser when selected folder is empty

--- projects/ui-components/lf-repository-browser/lf-repository-browser.component.ts
+++ projects/ui-components/lf-repository-browser/lf-repository-browser.component.ts
@@ -503,10 +503,11 @@ export class LfRepositoryBrowserComponent implements OnDestroy, AfterViewInit {
     if (!entry?.isContainer) {
       return;
     }
+    this._breadcrumbs = [entry].concat(this.breadcrumbs);
     this._currentFolder = entry;
     await this.updateAllPossibleEntriesAsync(entry);
-    this._breadcrumbs = [entry].concat(this.breadcrumbs);
-    this.entryList?.focus();
+    
+    this._focus();
   }
 
   /**
@@ -561,7 +562,13 @@ export class LfRepositoryBrowserComponent implements OnDestroy, AfterViewInit {
       setTimeout(() => this._focus(node, tries + 1));
       return;
     }
-    this.entryList.focus();
+    if (this.shouldShowEmptyMessage || this.shouldShowErrorMessage) {
+      setTimeout(() => {
+        (document.querySelector(".lf-repo-entry-container") as HTMLElement)?.focus();
+      })
+    } else {
+      this.entryList?.focus();
+    }
   }
 
   /**
