a13f00899bc5139edc077c7b75198d1c96e1e694
Shiyuan Zhao <shiyuan.zhao@laserfiche.com>
2026-06-26 16:56:35 -0700

    format

--- scripts/lint-sass-filenames.mjs
+++ scripts/lint-sass-filenames.mjs
@@ -1,5 +1,5 @@
-import { readdirSync, statSync } from 'fs';
-import { join, basename } from 'path';
+import { readdirSync, statSync } from "fs";
+import { join, basename } from "path";
 
 function walk(dir) {
   return readdirSync(dir).flatMap((name) => {
@@ -8,21 +8,21 @@ function walk(dir) {
   });
 }
 
-const SASS_DIRS = ['projects/styles/lf-laserfiche-lite/sass', 'projects/styles/lf-ms-office/sass'];
+const SASS_DIRS = ["projects/styles/lf-laserfiche-lite/sass", "projects/styles/lf-ms-office/sass"];
 
 const errors = [];
 
 for (const dir of SASS_DIRS) {
   for (const file of walk(dir)) {
     const name = basename(file);
-    if (!name.startsWith('_') && !name.startsWith('rwc-')) {
+    if (!name.startsWith("_") && !name.startsWith("rwc-")) {
       errors.push(`  ${file}: must start with '_' (common element partial) or 'rwc-' (component)`);
     }
   }
 }
 
 if (errors.length > 0) {
-  console.error('Sass filename violations:\n' + errors.join('\n'));
+  console.error("Sass filename violations:\n" + errors.join("\n"));
   process.exit(1);
 }
 
